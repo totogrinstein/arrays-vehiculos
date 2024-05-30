57073694a12a0cd8773c7fcf6db2c1a4c2b6ac81
totogrinstein <147621859+totogrinstein@users.noreply.github.com>
2024-05-30 15:51:10 -0300

    revision prueba, vehiculos

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            desactivarvehiculos();
+            vehiculos[Random.Range(0, 6)].SetActive(true);
+            
+        }   
+    }
+
+    void ResetearVehiculos()
+    {
+        desactivarvehiculos();
+    }
+
+    void desactivarvehiculos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+}
+
